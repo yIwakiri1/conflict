39227b7a4bef1a2826b51386b51eb710e65fde5b
test <live@any-plus.co.jp>
2023-05-19 15:40:44 +0900

    fewje

--- test.java
+++ test.java
@@ -2,9 +2,61 @@ import java.util.;
 
 interface A {
     abstract void x();
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}import java.util.;
+
+interface A {
+    abstract void x();
+}
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
 }
 
 abstract class B implements A {import java.util.;
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}import java.util.;
+
+interface A {
+    abstract void x();
+}
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
 
 interface A {
     abstract void x();
@@ -60,6 +112,146 @@ public class Main {
 
 interface A {
     abstract void x();
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}import java.util.;
+
+interface A {
+    abstract void x();
+}
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
+}
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}import java.util.;
+
+interface A {
+    abstract void x();
+}
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
+	
+	static void method() {
+	}
+}
+    public void x(){}
+}
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}import java.util.;
+
+interface A {
+    abstract void x();
+}
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List list = List.of(A, B, C); 不変
+        List list2 = Arrays.asList(A, B, C);  可変
+        var list3 = new ArrayList(Arrays.asList(new String[]{A, B, C}));    可変
+        ArrayListString list4 = new ArrayList(Arrays.asList(new String[]{A, B, C}));  可変
+        list8.set(0, hoge);
+        System.out.println(list8);
+    }
+	
+	static void method() {
+	}
+}import java.util.;
+
+interface A {
+    abstract void x();
 }
 
 abstract class B implements A {
@@ -82,7 +274,13 @@ public class Main {
 	
 	static void method() {
 	}
+}import java.util.;
+
+interface A {
+    abstract void x();
 }
+
+abstract class B implements A {
     public void x(){}
 }
 
@@ -102,4 +300,14 @@ public class Main {
 	
 	static void method() {
 	}
+}import java.util.;
+
+interface A {
+    abstract void x();
 }
+
+abstract class B implements A {
+    public void x(){}
+}
+
+
